b37503bff602e8214ca5eb17900825ddae854b30
ys3135@gmail.com <aiden-catius@catius.io>
2023-09-08 18:38:28 +0900

    [ADD] transaction readonly setting

---
+++ transaction-readonly/src/main/java/com/example/transactionreadonly/TransactionReadonlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionreadonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionReadonlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionReadonlyApplication.class, args);
+    }
+
+}
---
+++ transaction-readonly/src/test/java/com/example/transactionreadonly/TransactionReadonlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionreadonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionReadonlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
