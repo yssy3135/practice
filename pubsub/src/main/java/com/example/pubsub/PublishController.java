68358c1ec00b783644f1b5209259e9f07379c821
ys3135 <ys3135@gmail.com>
2024-01-08 23:34:37 +0900

    [ADD] redis pubsub

---
+++ pubsub/src/main/java/com/example/pubsub/PublishController.java
@@ -0,0 +1,22 @@
+package com.example.pubsub;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PublishController {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @PostMapping("events/users/deregister")
+    void publishDeregisterEvent() {
+
+        redisTemplate.convertAndSend("users:unregister", "500");
+
+    }
+
+
+}
