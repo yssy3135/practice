89d0db4d9ec082bef7419f82394fbc067321c304
yssy3135 <ys3135@gmail.com>
2024-01-28 19:29:14 +0900

    [MOD] readOnly test 깨진 부분 수정

---
+++ transaction-readonly/src/main/java/com/example/transactionreadonly/Member/service/dto/MemberResponse.java
@@ -0,0 +1,16 @@
+package com.example.transactionreadonly.Member.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class MemberResponse {
+
+    Long id;
+
+    String name;
+
+}
