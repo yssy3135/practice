3d3d7ae3239187415f81014552705a026e931445
yssy3135 <ys3135@gmail.com>
2024-02-01 22:43:25 +0900

    apply .gitignore

---
+++ fetch-join/src/main/java/com/example/fetchjoin/FetchJoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.fetchjoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchJoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchJoinApplication.class, args);
+    }
+
+}
---
+++ fetch-join/src/test/java/com/example/fetchjoin/FetchJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fetchjoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchJoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
